9ec8677dbe9813ad5f4de20d0c2d4eda9424e3c6
jose <jose.caravaca.atrapaguias@gmail.com>
2016-04-05 12:38:34 +0200

    Translation of error messages

--- eshop3/app/models/director.rb
+++ eshop3/app/models/director.rb
@@ -1,9 +1,19 @@
 class Director < ActiveRecord::Base
+
+    HUMANIZED_ATTRIBUTES = {
+    :first_name => "Nombre",
+    :last_name => "Apellidos"
+  }
+
+  def self.human_attribute_name(attr, options = {})
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
+
   #validates_presence_of :first_name, :last_name
-  validates :first_name, presence: { message: "Nombre no puede estar vacío" }
-  validates :last_name, presence:{ message: "Apellidos no puede estar vacío" }
+  validates :first_name,:last_name, presence:{ message: "no puede estar vacío" }
 
   def name
     "#{first_name} #{last_name}"
   end
+
 end
