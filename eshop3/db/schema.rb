9dd0e6aa6e93d3af42472fe0ecdf897a4ea59e08
Francisco Javier Ponce Rosales <franciscojavier.poncerosales@gmail.com>
2016-04-15 15:32:15 +0200

    Empezando a incluir Peliculas

--- eshop3/db/schema.rb
+++ eshop3/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411083206) do
+ActiveRecord::Schema.define(version: 20160412123806) do
 
   create_table "directors", force: true do |t|
     t.string   "first_name"
@@ -19,6 +19,34 @@ ActiveRecord::Schema.define(version: 20160411083206) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "directors_films", force: true do |t|
+    t.integer  "director_id", null: false
+    t.integer  "film_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "directors_films", ["director_id"], name: "fk_directors_films_directors", using: :btree
+  add_index "directors_films", ["film_id"], name: "fk_directors_films_films", using: :btree
+
+  create_table "films", force: true do |t|
+    t.string   "title",                               null: false
+    t.integer  "producer_id",                         null: false
+    t.datetime "produced_at"
+    t.string   "cod",                      limit: 13
+    t.text     "blurb"
+    t.integer  "duration"
+    t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "cover_image_file_name"
+    t.string   "cover_image_content_type"
+    t.integer  "cover_image_file_size"
+    t.datetime "cover_image_updated_at"
+  end
+
+  add_index "films", ["producer_id"], name: "fk_films_producers", using: :btree
 
   create_table "producers", force: true do |t|
     t.string   "name"
